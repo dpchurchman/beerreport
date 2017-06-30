d5249ce74e83c97e737204f8e0978002a2056ffc
dpchurchman <dchurchman@smu.edu>
2017-06-29 19:00:04 -0500

    Final draft

---
+++ BeerMerge.R
@@ -0,0 +1,11 @@
+#Change the name of Brew_ID to be identical in both sets
+names(breweries)[names(breweries)=="Brew_ID"]<-"Brewery_id"
+#Merge beers.csv and breweries.csv
+beermerge<-merge(beers, breweries, by = "Brewery_id")
+#Rename for clarity
+names(beermerge)[names(beermerge)=="Name.x"] <- "BeerName"
+names(beermerge)[names(beermerge)=="Name.y"] <- "BreweryName"
+#Order by state
+beermerge <- beermerge[order(beermerge$State),]
+#Trim extra white space
+beermerge$State <- trimws(beermerge$State)
